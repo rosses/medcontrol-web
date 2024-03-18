36c89b97a097882471b10057436d5ba539ae43c8
rossescl <rosses@gmail.com>
2024-03-18 17:00:11 -0300

    fix decimals

--- src/app/components/shared/session-complete/session-complete.component.ts
+++ src/app/components/shared/session-complete/session-complete.component.ts
@@ -61,7 +61,7 @@ export class SessionCompleteComponent implements OnInit {
   }
   numberOnly(event:any): boolean {
     const charCode = (event.which) ? event.which : event.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode != 188 && charCode != 190 && charCode != 110) {
       return false;
     }
     return true;
