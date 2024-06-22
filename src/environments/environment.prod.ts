669b4a8c60dfa7e4596e58a475e8028301929395
rossescl <rosses@gmail.com>
2024-06-22 14:26:31 -0400

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //url: 'https://api-medcontrol.enlanube.cl' //qa
-  url: 'https://medcontrol-api-cwnjx.ondigitalocean.app'
+  //url: 'https://medcontrol-api-cwnjx.ondigitalocean.app'
+  url: 'https://medcontrol-api.enlanube.cl',
 };
